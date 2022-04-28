bd6b442a8a144a59679f78640a3dcfe12eb17986
Rikard Fröberg <rikard@morus.se>
2022-04-28 23:03:10 +0200

    checkstyle

--- presentation-sources/Contact.java
+++ presentation-sources/Contact.java
@@ -1,61 +1,82 @@
-public class Contact{
+public class Contact {
+  
   private Email email;
   private Name name;
   private Phone phone;
 
-  public interface Exporter{
+  public interface Exporter {
     void addName(String name);
+
     void addEmail(String email);
+
     void addPhone(String phone);
   }
 
-  public interface Importer{
+  public interface Importer {
     String provideName();
+
     String provideEmail();
+
     String providePhone();
-    void   open();
-    void   close();
+
+    void open();
+
+    void close();
   }
 
   // Constructs a Contact from the builder provided
-  public Contact(Importer builder){
+  public Contact(Importer builder) {
     builder.open();
-    this.name  = new Name(builder.provideName());
+    this.name = new Name(builder.provideName());
     this.email = new Email(builder.provideEmail());
     this.phone = new Phone(builder.providePhone());
     builder.close();
   }
 
   // Exports this contact to the builder provided as argument
-  public void export(Exporter builder){
+  public void export(Exporter builder) {
     builder.addName(name.toString());
     builder.addEmail(email.toString());
     builder.addPhone(phone.toString());
   }
 
-  public String toString(){
+  public String toString() {
     return String.format("%s %s %s", name, email, phone); 
   }
 
-  private static class Name{
+  private static class Name {
     private String name;
-    public Name(String name){
+
+    public Name(String name) {
       this.name = name;
     }
-    public String toString(){ return name; }
+
+    public String toString() {
+      return name;
+    }
   }
-  private static class Email{
+  
+  private static class Email {
     private String email;
-    public Email(String email){
+    
+    public Email(String email) {
       this.email = email;
     }
-    public String toString(){ return email; }
+
+    public String toString() {
+      return email;
+    }
   }
-  private static class Phone{
+  
+  private static class Phone {
     private String phone;
-    public Phone(String phone){
+    
+    public Phone(String phone) {
       this.phone = phone;
     }
-    public String toString(){ return phone; }
+    
+    public String toString() {
+      return phone;
+    }
   }
 }
