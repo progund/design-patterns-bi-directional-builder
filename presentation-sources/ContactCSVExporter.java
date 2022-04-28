4c9ac9d567b7846874087abb6c95465b6397c592
Rikard Fröberg <rikard@morus.se>
2022-04-28 23:04:36 +0200

    checkstyle

--- presentation-sources/ContactCSVExporter.java
+++ presentation-sources/ContactCSVExporter.java
@@ -1,18 +1,23 @@
-public class ContactCSVExporter implements Contact.Exporter{
+public class ContactCSVExporter implements Contact.Exporter {
+  
   private String name;
   private String email;
   private String phone;
-  public void addName(String name){
+
+  public void addName(String name) {
     this.name = name;
   }
-  public void addEmail(String email){
+
+  public void addEmail(String email) {
     this.email = email;
   }
-  public void addPhone(String phone){
+
+  public void addPhone(String phone) {
     this.phone = phone;
   }
+
   @Override
-  public String toString(){
+  public String toString() {
     return new StringBuilder(name)
       .append(",")
       .append(email)
