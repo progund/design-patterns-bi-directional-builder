996bb77a90a3a04c20ae684dc9b950b2f88417c8
Rikard Fröberg <rikard@morus.se>
2022-04-28 23:14:35 +0200

    checkstyle

--- presentation-sources/TestContact.java
+++ presentation-sources/TestContact.java
@@ -1,10 +1,11 @@
-public class TestContact{
-  public static void main(String[] args){
-    Contact c=null;
-    if(args.length!=0){
-      if(args[0].equalsIgnoreCase("text")){
+public class TestContact {
+  
+  public static void main(String[] args) {
+    Contact c = null;
+    if (args.length != 0) {
+      if (args[0].equalsIgnoreCase("text")) {
         c = new Contact(new ContactTextImporter());
-      }else if(args[0].equalsIgnoreCase("gui")){
+      } else if (args[0].equalsIgnoreCase("gui")) {
         c = new Contact(new ContactSwingImporter());
       }
     }
@@ -15,7 +16,7 @@ public class TestContact{
     System.out.println(html);
     ContactCSVExporter csv = new ContactCSVExporter();
     c.export(csv);
-    System.out.println("CSV:\n"+csv);
+    System.out.println("CSV:\n" + csv);
     System.out.println("Starting gui display...");
     GUIExporter gui = new GUIExporter();
     c.export(gui);
